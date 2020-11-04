9a09b881c457f2563470522239be0a33a549fa94
HH <HH@DESKTOP-9AQQE68>
2020-11-04 22:05:55 +0900

    commit

---
+++ src/main/java/board/MsaBoardApplication.java
@@ -0,0 +1,13 @@
+package board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaBoardApplication.class, args);
+	}
+
+}
